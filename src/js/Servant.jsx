ee8f7c0addda022d0fe2d8ac57b475e2af3f10e0
Roadagain <ringoh72@gmail.com>
2018-03-08 20:24:07 +0900

    NPも変更可能にする

--- src/js/Servant.jsx
+++ src/js/Servant.jsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
 export default class Servant extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: props.name,
+            np: 0
+        };
+    }
+
+    setNP(np) {
+        np = Math.max(Math.min(np, 100), 0);
+        if (Number.isNaN(np)) {
+            return;
+        }
+        this.setState({np});
+    }
+
     render() {
         return <div>
         <p>
         <input type="text"></input>
-        <progress value={this.props.np} max="100">{this.props.np}%</progress>
-        {this.props.np}%
+        <progress value={this.state.np} max="100">{this.state.np}%</progress>
+        <input type="number" defaultValue={this.state.np} onChange={e => this.setNP(e.target.value)}></input>%
         </p>
         </div>
     }
