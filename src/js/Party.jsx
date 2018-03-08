2fa1af956e678c9d4dd5515ff993618e30bce536
Roadagain <ringoh72@gmail.com>
2018-03-08 22:52:32 +0900

    鯖情報も入れ替えられるように全部Partyで持つ

--- src/js/Party.jsx
+++ src/js/Party.jsx
@@ -4,9 +4,13 @@ import Servant from './Servant';
 export default class Party extends React.Component {
     constructor(props) {
         super(props);
+        const servantsInfo = props.servants.map(name => ({
+            name,
+            np: 0
+        }));
         this.state = {
-            starting: props.servants.slice(0, 3),
-            sub: props.servants.slice(3),
+            starting: servantsInfo.slice(0, 3),
+            sub: servantsInfo.slice(3),
             orderedStarting: null,
             orderedSub: null
         };
@@ -24,19 +28,49 @@ export default class Party extends React.Component {
         this.setState({starting, sub});
     }
 
+    setName(index, name) {
+        if (index < 3) {
+            const starting = this.state.starting;
+            starting[index].name = name;
+            this.setState({starting});
+        }
+        else {
+            const sub = this.state.sub;
+            sub[index - 3].name = name;
+            this.setState({sub});
+        }
+    }
+
+    setNP(index, np) {
+        np = Math.max(Math.min(np, 300), 0);
+        if (Number.isNaN(np)) {
+            return;
+        }
+        if (index < 3) {
+            const starting = this.state.starting;
+            starting[index].np = np;
+            this.setState({starting});
+        }
+        else {
+            const sub = this.state.sub;
+            sub[index - 3].np = np;
+            this.setState({sub});
+        }
+    }
+
     render() {
         return <div>
         <ol>
-        {this.state.starting.map((name, index) => {
+        {this.state.starting.map(({name, np}, index) => {
             return <li>
                 <input type="radio" name="starting" value={index} onClick={e => this.setState({orderedStarting: e.target.value})}/>
-                <Servant name={name} np={0} index={index}/>
+                <Servant name={name} np={np} index={index} onNameChanged={name => this.setName(index, name)} onNPChanged={np => this.setNP(index, np)}/>
             </li>;
         })}
-        {this.state.sub.map((name, index) => {
+        {this.state.sub.map(({name, np}, index) => {
             return <li>
                 <input type="radio" name="sub" value={index} onClick={e => this.setState({orderedSub: e.target.value})}/>
-                <Servant name={name} np={0} index={index}/>
+                <Servant name={name} np={np} index={index} onNameChanged={name => this.setName(index + 3, name)} onNPChanged={np => this.setNP(index + 3, np)}/>
             </li>;
         })}
         </ol>
--- src/js/Servant.jsx
+++ src/js/Servant.jsx
@@ -1,35 +1,19 @@
 import React from 'react';
 
 export default class Servant extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: props.name,
-            np: 0
-        };
-    }
-
-    setNP(np) {
-        np = Math.max(Math.min(np, 300), 0);
-        if (Number.isNaN(np)) {
-            return;
-        }
-        this.setState({np});
-    }
-
     liberateNoblePhantasm() {
-        if (this.state.np >= 100) {
-            this.setState({np: 0});
+        if (this.props.np >= 100) {
+            this.props.onNPChanged(0);
         }
     }
 
     render() {
         return <div>
         <p>
-        <input type="text" placeholder={this.props.name} onChange={e => this.setState({name: e.target.Value})} />
-        <progress value={this.state.np} max="100">{this.state.np}%</progress>
-        <input type="number" value={this.state.np} onChange={e => this.setNP(e.target.value)} />%
-        <button onClick={() => this.liberateNoblePhantasm()} disabled={this.props.index > 2 || this.state.np < 100}>宝具</button>
+        <input type="text" value={this.props.name} onChange={e => this.props.onNameChanged(e.target.value)} />
+        <progress value={this.props.np} max="100">{this.props.np}%</progress>
+        <input type="number" value={this.props.np} onChange={e => this.props.onNPChanged(e.target.value)} />%
+        <button onClick={() => this.liberateNoblePhantasm()} disabled={this.props.index > 2 || this.props.np < 100}>宝具</button>
         </p>
         </div>
     }
