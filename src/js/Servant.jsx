e4f0c863795c213faa662004b2528a998ec3e87d
Roadagain <ringoh72@gmail.com>
2018-03-08 20:31:49 +0900

    宝具使用ボタンを実装

--- src/js/Servant.jsx
+++ src/js/Servant.jsx
@@ -17,12 +17,19 @@ export default class Servant extends React.Component {
         this.setState({np});
     }
 
+    useNoblePhantasm() {
+        if (this.state.np >= 100) {
+            this.setState({np: 0});
+        }
+    }
+
     render() {
         return <div>
         <p>
         <input type="text"></input>
         <progress value={this.state.np} max="100">{this.state.np}%</progress>
         <input type="number" value={this.state.np} onChange={e => this.setNP(e.target.value)}></input>%
+        <button onClick={() => this.useNoblePhantasm()}>宝具</button>
         </p>
         </div>
     }
