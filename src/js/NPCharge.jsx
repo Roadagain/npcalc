4d808ecb827dc224000178bf57cc8654f3b5b78c
Roadagain <ringoh72@gmail.com>
2018-03-09 00:47:26 +0900

    NPチャージ用のコンポーネントを作成

---
+++ src/js/NPCharge.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Target from './Target';
+
+export default class NPCharge extends React.Component {
+    render() {
+        return <div>
+            {Target.asString(this.props.target)}
+            <input type="number" value={this.props.charge} onChange={e => this.props.onChargeChange(this.props.target, e.target.value)}/>%
+            <button onClick={this.props.onNPCharge.bind(this, this.props.target, this.props.charge)}>付与</button>
+        </div>;
+    }
+}
