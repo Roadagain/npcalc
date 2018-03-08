13a6e3122318fd19ffad6912f0cf4058af912f52
Roadagain <ringoh72@gmail.com>
2018-03-09 00:00:27 +0900

    NPチャージ用のコンポーネントに値を渡す準備

--- src/js/Party.jsx
+++ src/js/Party.jsx
@@ -6,7 +6,8 @@ export default class Party extends React.Component {
         super(props);
         const servantsInfo = props.servants.map(name => ({
             name,
-            np: 0
+            np: 0,
+            charge: [0, 0, 0]
         }));
         this.state = {
             starting: servantsInfo.slice(0, 3),
@@ -58,19 +59,35 @@ export default class Party extends React.Component {
         }
     }
 
+    onChargeChange(index, target, charge) {
+        if (Number.isNaN(charge)) {
+            return;
+        }
+        if (index < 3) {
+            const starting = this.state.starting;
+            starting[index].charge[target] = charge;
+            this.setState({starting});
+        }
+        else {
+            const sub = this.state.sub;
+            sub[index - 3].charge[target] = charge;
+            this.setState({sub});
+        }
+    }
+
     render() {
         return <div>
         <ol>
-        {this.state.starting.map(({name, np}, index) => {
+        {this.state.starting.map(({name, np, charge}, index) => {
             return <li>
                 <input type="radio" name="starting" value={index} onClick={e => this.setState({orderedStarting: e.target.value})}/>
-                <Servant name={name} np={np} index={index} onNameChanged={name => this.setName(index, name)} onNPChanged={np => this.setNP(index, np)}/>
+                <Servant name={name} np={np} charge={charge} index={index} onNameChanged={name => this.setName(index, name)} onNPChanged={np => this.setNP(index, np)} onChargeChange={this.onChargeChange} />
             </li>;
         })}
-        {this.state.sub.map(({name, np}, index) => {
+        {this.state.sub.map(({name, np, charge}, index) => {
             return <li>
                 <input type="radio" name="sub" value={index} onClick={e => this.setState({orderedSub: e.target.value})}/>
-                <Servant name={name} np={np} index={index + 3} onNameChanged={name => this.setName(index + 3, name)} onNPChanged={np => this.setNP(index + 3, np)}/>
+                <Servant name={name} np={np} charge={charge} index={index + 3} onNameChanged={name => this.setName(index + 3, name)} onNPChanged={np => this.setNP(index + 3, np)} onChargeChange={this.onChargeChange} />
             </li>;
         })}
         </ol>
