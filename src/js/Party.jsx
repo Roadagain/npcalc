9a03af8a97c6382e87b3ef16f9dbc87330ae82e7
Roadagain <ringoh72@gmail.com>
2018-03-08 21:25:10 +0900

    宝具打てないときは宝具ボタンを殺す

--- src/js/Party.jsx
+++ src/js/Party.jsx
@@ -5,7 +5,7 @@ export default class Party extends React.Component {
     render() {
         return <ol>
         {this.props.servants.map((name, idx) => {
-            return <li><Servant name={name} np={0}/></li>;
+            return <li><Servant name={name} np={0} index={idx}/></li>;
         })}
         </ol>;
     }
