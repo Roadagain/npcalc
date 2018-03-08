d23a2b32b23255c3ccb3830504645ba0668e14fa
Roadagain <ringoh72@gmail.com>
2018-03-08 19:34:36 +0900

    鯖情報は1行に収める

--- src/js/Servant.jsx
+++ src/js/Servant.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default class Servant extends React.Component {
     render() {
         return <div>
-        <p>{this.props.name}</p>
         <p>
+        {this.props.name}
         <progress value={this.props.np} max="100">{this.props.np}%</progress>
         {this.props.np}%
         </p>
