0e050a1998ecdbdcb5e62126ba3cc69f72c7b35f
Roadagain <ringoh72@gmail.com>
2018-03-09 12:50:25 +0900

    長い変数を別変数に入れて短縮

--- src/js/Servant.jsx
+++ src/js/Servant.jsx
@@ -10,25 +10,27 @@ export default class Servant extends React.Component {
     }
 
     render() {
-        const onChargeChange = (target, charge) => this.props.onChargeChange(this.props.index, target, charge);
-        const onNPCharge = (target, charge) => this.props.onNPCharge(this.props.index, target, charge);
+        const index = this.props.index;
+        const charge = this.props.charge;
+        const onChargeChange = (target, charge) => this.props.onChargeChange(index, target, charge);
+        const onNPCharge = (target, charge) => this.props.onNPCharge(index, target, charge);
 
         return <div>
             <p>
                 <input type="text" value={this.props.name} onChange={e => this.props.onNameChanged(e.target.value)} />
                 <progress value={this.props.np} max="100">{this.props.np}%</progress>
                 <input type="number" value={this.props.np} onChange={e => this.props.onNPChanged(e.target.value)} />%
-                <button onClick={() => this.liberateNoblePhantasm()} disabled={this.props.index > 2 || this.props.np < 100}>宝具</button>
+                <button onClick={() => this.liberateNoblePhantasm()} disabled={index > 2 || this.props.np < 100}>宝具</button>
             </p>
             <ul>
                 <li key={0}>
-                    <NPCharge index={this.props.index} target={Target.SELF} charge={this.props.charge[Target.SELF]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
+                    <NPCharge index={index} target={Target.SELF} charge={charge[Target.SELF]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
                 </li>
                 <li key={1}>
-                    <NPCharge index={this.props.index} target={Target.SOMEONE} charge={this.props.charge[Target.SOMEONE]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
+                    <NPCharge index={index} target={Target.SOMEONE} charge={charge[Target.SOMEONE]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
                 </li>
                 <li key={2}>
-                    <NPCharge index={this.props.index} target={Target.ALL} charge={this.props.charge[Target.ALL]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
+                    <NPCharge index={index} target={Target.ALL} charge={charge[Target.ALL]} onChargeChange={onChargeChange} onNPCharge={onNPCharge} />
                 </li>
             </ul>
         </div>;
