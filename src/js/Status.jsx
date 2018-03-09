7d3599c3dc2585a0b0f0f795158cac3d7e3420eb
Roadagain <ringoh72@gmail.com>
2018-03-09 14:53:16 +0900

    SCSSのセレクタをそれっぽく修正

--- src/js/Status.jsx
+++ src/js/Status.jsx
@@ -12,9 +12,9 @@ export default class Status extends React.Component {
         const np = this.props.np;
 
         return <div>
-            <input type="text" value={this.props.name} onChange={e => this.props.onNameChange(index, e.target.value)} className="longtext" />
+            <input type="text" value={this.props.name} onChange={e => this.props.onNameChange(index, e.target.value)} className="long-text" />
             <progress value={np} max="100" className="np-guage">{np}%</progress>
-            <input type="number" value={np} onChange={e => this.props.onNPChange(index, e.target.value)} className="shorttext" />%
+            <input type="number" value={np} onChange={e => this.props.onNPChange(index, e.target.value)} className="short-text" />%
             <button onClick={() => this.liberateNoblePhantasm()} disabled={index > 2 || np < 100}>宝具</button>
         </div>;
     }
