85e4dbdf5cdd8c4216e4ba8017478cf53783ee24
Roadagain <ringoh72@gmail.com>
2018-03-11 23:49:18 +0900

    マスター礼装をラジオボタンで切り替え

---
+++ src/js/Master.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Target from './Target';
+import Uniform from './Uniform';
+import UniformRadio from './UniformRadio';
+
+export default class Master extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            uniform: Uniform.EMPTY
+        };
+    }
+
+    onUniformChange(uniform) {
+        this.setState({uniform});
+    }
+
+    render() {
+        const implemented = [
+            Uniform.EMPTY,
+            new Uniform('カルデア戦闘服', this.props.orderChange, 'オーダーチェンジ'),
+            new Uniform('魔術協会制服', this.props.onNPCharge.bind(this, null, Target.SOMEONE, 20), '霊子譲渡'),
+        ];
+        const onUniformChange = this.onUniformChange.bind(this);
+
+        return <div>
+            <form>
+                {implemented.map((uniform, index) => {
+                    const checked = uniform.name === this.state.uniform.name;
+                    return <UniformRadio {...{uniform, onUniformChange, checked}} key={"uniform" + index} />;
+                })}
+            </form>
+            {this.state.uniform.skill !== null ? <button onClick={this.state.uniform.skill}>{this.state.uniform.skillName}</button> : null}
+        </div>
+    }
+}
