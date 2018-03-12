8646cf5bab729a388af606ca9a13c9b977b59e48
Roadagain <ringoh72@gmail.com>
2018-03-12 11:20:20 +0900

    0%になった時は内部テキストをなくす

--- src/js/NPCharge.jsx
+++ src/js/NPCharge.jsx
@@ -7,7 +7,7 @@ export default class NPCharge extends React.Component {
         const onNPCharge = this.props.onNPCharge.bind(this, this.props.target, this.props.charge);
         return <div>
             {Target.asString(this.props.target)}
-            <input type="number" placeholder={0} onChange={onChargeChange} className="short-text"/>%
+            <input type="number" placeholder={0} value={this.props.charge || ""} onChange={onChargeChange} className="short-text"/>%
             <button onClick={onNPCharge} disabled={this.props.index >= 3 || this.props.charge === 0}>付与</button>
         </div>;
     }
