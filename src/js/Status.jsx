fd9b03d610f44d5ce457d72dd3b8f4c038009b67
Roadagain <ringoh72@gmail.com>
2018-03-12 11:13:26 +0900

    数値デフォルトの0はプレースホルダーに変更

--- src/js/Status.jsx
+++ src/js/Status.jsx
@@ -14,7 +14,7 @@ export default class Status extends React.Component {
         return <div>
             <input type="text" value={this.props.name} onChange={e => this.props.onNameChange(index, e.target.value)} className="long-text" />
             <progress value={np} max="100" className="np-guage">{np}%</progress>
-            <input type="number" value={np} onChange={e => this.props.onNPChange(index, e.target.value)} className="short-text" />%
+            <input type="number" placeholder={0} onChange={e => this.props.onNPChange(index, e.target.value)} className="short-text" />%
             <button onClick={() => this.liberateNoblePhantasm()} disabled={index > 2 || np < 100}>宝具</button>
         </div>;
     }
