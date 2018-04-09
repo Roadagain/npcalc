e8fd753e6e392cfef6a0faa700e920398434639d
Roadagain <ringoh72@gmail.com>
2018-04-09 22:47:22 +0900

    数値フォーム表示で改行しない

--- src/js/NumberInput.jsx
+++ src/js/NumberInput.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default class NumberInput extends React.Component {
     render() {
-        return <div>
+        return <form className="number">
             {/*チャージ値が0ならプレースホルダーを表示する(入力の時に050とかにならないため)*/}
             <input type="number" placeholder={0} value={this.props.value || ""} onChange={this.props.onChange} className="short-text"/>%
-        </div>;
+        </form>;
     }
 }
