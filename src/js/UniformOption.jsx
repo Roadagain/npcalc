59c3ffcdf8e37e3d363c0e11708ac4c442b59227
Roadagain <ringoh72@gmail.com>
2018-05-17 22:38:43 +0900

    爆発四散させて再構築

--- src/js/UniformOption.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class UniformOption extends React.Component {
-    render() {
-        return <option value={this.props.name}>
-            {this.props.name}
-        </option>;
-    }
-}
