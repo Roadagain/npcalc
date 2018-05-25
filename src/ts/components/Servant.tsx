0ddb5be77f00188e873d49aa714acd4b38b2ca5a
Roadagain <ringoh72@gmail.com>
2018-05-26 01:45:37 +0900

    NPゲージを追加

--- src/ts/components/Servant.tsx
+++ src/ts/components/Servant.tsx
@@ -1,5 +1,6 @@
 import { GridList, TextField, Theme, withStyles, WithStyles } from '@material-ui/core';
 import React from 'react';
+import NPGuage from './NPGuage';
 import Skill, { ISkillProps } from './Skill';
 import { wrapGridListTile } from './Util';
 
@@ -19,6 +20,7 @@ function Servant({ classes, name, skills }: IServantProps & WithStyles) {
     return (
         <div className={classes.container}>
         <TextField value={name} />
+        <NPGuage np={100} />
         <GridList cols={3}>
             {skills.map(skill => wrapGridListTile(<Skill {...skill} />, 1, skill.name))}
         </GridList>
