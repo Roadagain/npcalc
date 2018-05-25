85d8fbb0ee48d917aec39da62314a555116921c6
Roadagain <ringoh72@gmail.com>
2018-05-26 00:07:33 +0900

    サーヴァントとスキルの両端にマージンを取る

--- src/ts/components/Servant.tsx
+++ src/ts/components/Servant.tsx
@@ -1,16 +1,23 @@
-import { GridList, TextField } from '@material-ui/core';
+import { GridList, TextField, Theme, withStyles, WithStyles } from '@material-ui/core';
 import React from 'react';
 import Skill, { ISkillProps } from './Skill';
 import { wrapGridListTile } from './Util';
+
+const styles = (theme: Theme) => ({
+    container: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+});
 
 export interface IServantProps {
     name: string;
     skills: ISkillProps[];
 }
 
-export default function Servant({ name, skills }: IServantProps) {
+function Servant({ classes, name, skills }: IServantProps & WithStyles) {
     return (
-        <div>
+        <div className={classes.container}>
         <TextField value={name} />
         <GridList cols={3}>
             {skills.map(skill => wrapGridListTile(<Skill {...skill} />, 1, skill.name))}
@@ -18,3 +25,5 @@ export default function Servant({ name, skills }: IServantProps) {
         </div>
     );
 }
+
+export default withStyles(styles)(Servant);
--- src/ts/components/Skill.tsx
+++ src/ts/components/Skill.tsx
@@ -1,10 +1,19 @@
-import { TextField } from '@material-ui/core';
+import { TextField, Theme, withStyles, WithStyles } from '@material-ui/core';
 import React from 'react';
+
+const styles = (theme: Theme) => ({
+    container: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit,
+    },
+});
 
 export interface ISkillProps {
     name: string;
 }
 
-export default function Skill({ name }: ISkillProps) {
-    return <TextField value={name} />;
+function Skill({ classes, name }: ISkillProps & WithStyles) {
+    return <TextField value={name} className={classes.container} />;
 }
+
+export default withStyles(styles)(Skill);
