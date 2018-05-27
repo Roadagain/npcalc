d6b61b665c4751db0ac429acfebad368232b5a63
Roadagain <ringoh72@gmail.com>
2018-05-27 14:48:26 +0900

    サーヴァントコンポーネントの幅を広げる

--- src/ts/components/Party.tsx
+++ src/ts/components/Party.tsx
@@ -10,13 +10,13 @@ export interface IPartyProps {
 export default function Party({ servants }: IPartyProps) {
     const [forwards, backwards] = [servants.slice(0, 3), servants.slice(3)];
     return (
-        <GridList cols={12} cellHeight="auto">
-            <GridListTile cols={1.5} />
-            {forwards.map(servant => wrapGridListTile(<Servant {...servant} />, 3, servant.name))};
-            <GridListTile cols={1.5} />
-            <GridListTile cols={1.5} />
-            {backwards.map(servant => wrapGridListTile(<Servant {...servant} />, 3, servant.name))};
-            <GridListTile cols={1.5} />
+        <GridList cols={14} cellHeight="auto">
+            <GridListTile cols={1} />
+            {forwards.map(servant => wrapGridListTile(<Servant {...servant} />, 4, servant.name))};
+            <GridListTile cols={1} />
+            <GridListTile cols={1} />
+            {backwards.map(servant => wrapGridListTile(<Servant {...servant} />, 4, servant.name))};
+            <GridListTile cols={1} />
         </GridList>
     );
 }
