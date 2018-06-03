4b020c689036b230c5f04f9cd8f4831a4a024c04
Roadagain <ringoh72@gmail.com>
2018-06-03 22:54:55 +0900

    props用インターフェースの名前にちゃんとサフィックスつける

--- src/ts/components/SuffixedNumberInput.tsx
+++ src/ts/components/SuffixedNumberInput.tsx
@@ -1,12 +1,12 @@
 import { Input, InputAdornment } from '@material-ui/core';
 import React from 'react';
 
-export interface ISuffixedNumberInput {
+export interface ISuffixedNumberInputProps {
     value: number;
     suffix: string;
 }
 
-export default function SuffixedNumberInput({ value, suffix }: ISuffixedNumberInput) {
+export default function SuffixedNumberInput({ value, suffix }: ISuffixedNumberInputProps) {
     const endAdornment = <InputAdornment position="end">{suffix}</InputAdornment>;
     return <Input {...{ value, endAdornment }} type="number" fullWidth={true} />;
 }
