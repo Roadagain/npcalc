c4ea5f05941365c001ebc9068b9ad9b0a78c9e52
Roadagain <ringoh72@gmail.com>
2018-06-19 20:56:13 +0900

    不要import削除

--- src/ts/components/Title.tsx
+++ src/ts/components/Title.tsx
@@ -1,6 +1,5 @@
-import { Card, CardContent, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 import React from 'react';
-import CardWrapper from './CardWrapper';
 
 export default function Title() {
     return (
